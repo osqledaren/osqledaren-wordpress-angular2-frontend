f2cb36a6b8240586a74e1b59e8f93ff8fba50e5d
Tedy Warsitha <tedy@kth.se>
2016-11-16 14:52:00 +0100

    Refactored and added library container

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// Definitions
+define('OSQLEDAREN_HEADLESS_LIB', get_stylesheet_directory() . '/lib');
+
+// Include library
+include_once OSQLEDAREN_HEADLESS_LIB . '/custom-post-types.php';
