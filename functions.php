998d674e27b4b70c07e3e1153509579012329853
Tedy Warsitha <tedy@kth.se>
2017-02-19 12:03:16 +0100

    Added post thumbnail support and updated gitignore.

--- functions.php
+++ functions.php
@@ -4,4 +4,4 @@
 define('OSQLEDAREN_HEADLESS_LIB', get_stylesheet_directory() . '/lib');
 
 // Include library
-include_once OSQLEDAREN_HEADLESS_LIB . '/custom-post-types.php';
+include_once OSQLEDAREN_HEADLESS_LIB . '/hooks.php';
